dc8795d3fd09134ed66a95d6043a7b265fbc99aa
iCrazeiOS <39101269+iCrazeiOS@users.noreply.github.com>
2020-03-30 14:45:56 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(stripos($_SERVER['HTTP_USER_AGENT'],"iPod") === false && stripos($_SERVER['HTTP_USER_AGENT'],"iPhone") === false && stripos($_SERVER['HTTP_USER_AGENT'],"iPad") === false){
-	$version = 'Not on iOS';
-} else {
-	$version = preg_replace("/(.*) OS ([0-9]*)_(.*)/","$2", $_SERVER['HTTP_USER_AGENT']);
-}
-echo $version;
